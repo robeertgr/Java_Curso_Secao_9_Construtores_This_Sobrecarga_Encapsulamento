7080615d81f364ac9c45e0a49c876df3db5d0173
Robert Granja <robeertgr@live.com>
2022-03-07 11:44:40 -0300

    Refatoração no nome da classe e acréscimo do construtor.

--- src/application/Main_Product.java
+++ src/application/Main_Product.java
@@ -5,24 +5,23 @@ import entities.Product;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class Main_Product {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Product product = new Product();
-
         System.out.println("Insira os dados do produto: ");
         System.out.print("Nome: ");
-        product.name = sc.nextLine();
+        String name = sc.nextLine();
         System.out.print("Preço: ");
-        product.price = sc.nextDouble();
+        double price = sc.nextDouble();
         System.out.print("Quantidade em estoque: ");
-        product.quantity = sc.nextInt();
+        int quantity = sc.nextInt();
+        Product product = new Product(name, price, quantity);
 
         System.out.println("\nDados do produto: " + product);
         System.out.print("\nInsira a quantidade de produtos a serem adicionados em estoque: ");
-        int quantity = sc.nextInt();
+        quantity = sc.nextInt();
         product.addProducts(quantity);
 
         System.out.println("\nDados atualizados: " + product);
